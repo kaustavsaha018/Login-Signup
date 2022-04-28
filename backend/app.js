258ed76e9e522ab0aecf39570133be12a2fe4b7e
nmbaibhab <nmbaibhab@gmail.com>
2022-04-28 21:58:35 +0530

    "user data set to local storage"

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,21 @@
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const express = require('express');
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
+app.use(express.json({ limit: "30mb" }));
+dotenv.config({ path: "./config.env" });
 
-
-const cors = require('cors');  app.use(cors());
-
-dotenv.config({path: './config.env'});
-require('./db/conn');
-//const User = require('./model/userSchema');
+require("./db/conn");
+// const User = require('./model/userSchema');
 app.use(express.json());
-app.use(require('./router/auth'));
-const PORT = process.env.PORT; 
+app.use("/", require("./router/auth"));
+const PORT = process.env.PORT;
 
-//Middleware
+// Middleware
 // const middleWare = (res, req, next) => {
 //     console.log('Hello this is my middleware');
 //     next();
@@ -25,15 +27,16 @@ const PORT = process.env.PORT;
 // app.get('/about', (req, res) => {
 //     res.send(`This is about my project`);
 // })
-app.get('/contact', (req, res) => {
-    res.send(`Feel free to contact me if needed`);
-})
-app.get('/login', (req, res) => {
-    res.send(`This is LOGIN Page`);
-})
-app.get('/signup', (req, res) => {
-    res.send(`This is SIGNUP Page`);
-})
-app.listen(PORT, () =>{
-    console.log(`Server is Running at port ${PORT}`)
-})
+
+// app.get("/contact", (req, res) => {
+//   res.send(`Feel free to contact me if needed`);
+// });
+// app.get("/login", (req, res) => {
+//   res.send(`This is LOGIN Page`);
+// });
+// app.get("/signup", (req, res) => {
+//   res.send(`This is SIGNUP Page`);
+// });
+app.listen(PORT, () => {
+  console.log(`Server is Running at port ${PORT}`);
+});
